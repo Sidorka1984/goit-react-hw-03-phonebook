e400153b9852d9eb6441a66c7ab380ae24d92d2f
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-07-09 11:46:57 +0300

    перенесла данные с предыдущей роботы

---
+++ src/component/Container/Container.jsx
@@ -0,0 +1,14 @@
+import PropType from 'prop-types';
+import { Containers, Title } from './Container.styles';
+import { FaBookOpen } from 'react-icons/fa';
+
+const Container = ({ title, children }) => (
+    <Containers>
+        <Title><FaBookOpen /> {title}</Title>
+        {children}
+    </Containers>
+);
+Container.propType = {
+    title: PropType.string.isRequired,
+};
+export default Container;
